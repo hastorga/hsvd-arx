0f4f214126d948b64d95a7b199e2f3b5b8fb3103
Elias Araya Diaz <elias@MacBook-Air-de-Elias.local>
2019-04-24 00:26:33 -0400

    Changes in main to set memory

--- main.m
+++ main.m
@@ -1,11 +1,11 @@
 Y = load("DataAR.dat");
 #Y = [1,2,3,4,5,6,7,8,9,10];
 B = Y';
-n = 1;
+n = 10;
 acum = 0;
+h = 20;
 
 for i = 1:n
-
 #Se obtienen los valores de alta y baja frecuencia que son retornados por la función hsvd 
 [B,A] = hsvd(B);
 acum = acum + A; # Suma de las altas frecuencias
@@ -17,9 +17,11 @@ X = B + acum;
 mean((X-Y).^2);
 
 # Se obtienen los valores estimados de las frecuencias mediante la AR
-l = ar(B);
-h = ar(acum);
+l = ar(B,h);
+h = ar(acum,h);
 final = l+h;
-
+figure(1)
+plot(Y)
+figure(2)
 plot(final)
 
