51bd6f121e126ccb9633eb7a2dc36957ae6d4bbf
Unknown <hm.astorga@gmail.com>
2019-05-09 00:34:00 -0400

    feat: some changes

--- main.m
+++ main.m
@@ -1,22 +1,26 @@
 Y = load("DataGrupo1.dat");
+#Y = [1;2;3;4;5;6;7;8;9;10];
 
 memo = 30;
 erVec = [];
-
-for z = 1:memo
-#Y = [1;2;3;4;5;6;7;8;9;10]; # Data ejeplo easy
-n = 10; # niveles de descomposicion
-horizonte = 1; #horizonte
-h = z; # lag
-hrow = 2; # filas hankel
 trainpercent = 0.8;
-acum = 0;
-B = Y';
 
-matrix_training_size = int16(size(Y,1)* trainpercent)+1
+matrix_training_size = int16(size(Y,1)* trainpercent)
 training_data = Y(1:matrix_training_size);
 
 test_data = Y(matrix_training_size:end);
+
+for z = 1:memo
+#Y = [1;2;3;4;5;6;7;8;9;10]; # Data ejeplo easy
+n = 5; # niveles de descomposicion
+horizonte = 1; #horizonte
+h = z; # lag
+hrow = 2; # filas hankel
+acum = 0;
+
+
+B = Y'; 
+#B = training_data';
 
 for i = 1:n
 #Se obtienen los valores de alta y baja frecuencia que son retornados por la función hsvd 
@@ -29,16 +33,17 @@ endfor
 X = B + acum;
 
 # Se obtienen los valores estimados de las frecuencias mediante la AR
-lf = ar(B,h, horizonte,training_data);
+lf = ar(B,h, horizonte,trainpercent); 
 arx = [B acum];
-hf = ar(arx,h, horizonte,training_data);
+hf = ar(arx,h, horizonte,trainpercent);
 
-final = hf+lf;
+final = hf+lf
 figure(1)
 cla()
-plot(Y(size(Y,1)-117:end,1))
+#plot(Y(size(Y,1)-117:end,1))
+plot(Y(1:end))
 hold on
-plot(final, "r")
+plot(final(1:end), "r")
 
 
 #x = [real,final] 
@@ -47,7 +52,9 @@ plot(final, "r")
 
 
 
-real = Y(size(Y,1)-117:end,1);
+#real = Y(size(Y,1)-117:end,1);
+real = Y(size(Y,1)-size(test_data,1) +1: end,1)
+
 e = real-final;
 err = mean(real-final).^2;
 rmse = err.^0.5;
